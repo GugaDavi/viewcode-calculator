19853346d7089c42750f66298bb0fb795c91238b
Gustavo Guedes <gguedes.tech@gmail.com>
2024-08-23 22:07:18 -0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Gustavo Guedes on 23/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
