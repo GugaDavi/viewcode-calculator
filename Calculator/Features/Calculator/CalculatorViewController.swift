aeed52b092e6455fe28cfedaee69d7c9bc0986cc
Gustavo Guedes <gguedes.tech@gmail.com>
2024-09-05 20:10:01 -0300

    initial unit tests

--- Calculator/Features/Calculator/CalculatorViewController.swift
+++ Calculator/Features/Calculator/CalculatorViewController.swift
@@ -9,15 +9,35 @@ import UIKit
 
 final class CalculatorViewController: UIViewController {
 	private var calculatorView: CalculatorView? = nil
+	private var calculatorViewModel = CalculatorViewModel()
+	
+	var currentValue: Double = 0
+	var leftValue: Double = 0
+	var rightValue: Double = 0
+	var action: ActionType? = nil
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		buildView()
+		calculatorView?.buttonsDelegate = self
+		calculatorViewModel = CalculatorViewModel()
 	}
 	
-	private func buildView() {
+	override func loadView() {
 		view = CalculatorView()
 		calculatorView = view as? CalculatorView
+		calculatorView?.setup()
+	}
+}
+
+//MARK: - CalculatorButtonViewDelegate
+
+extension CalculatorViewController: CalculatorButtonViewDelegate {
+	func didTapButton(_ sender: UIButton) {
+		if let buttonValue = sender.currentTitle {
+			let newValue = calculatorViewModel.didTapButton(buttonValue: buttonValue)
+			
+			calculatorView?.updateResult(newValue)
+		}
 	}
 }
