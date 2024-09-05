5f8c557f6138df7a27f8c2fa24a033c63563df26
Gustavo Guedes <gguedes.tech@gmail.com>
2024-09-05 20:06:31 -0300

    improve calculator view fields declarations

--- Calculator/Features/Calculator/View/CalculatorButtonView.swift
+++ Calculator/Features/Calculator/View/CalculatorButtonView.swift
@@ -7,54 +7,93 @@
 
 import UIKit
 
+protocol CalculatorButtonViewDelegate: AnyObject {
+	func didTapButton(_ sender: UIButton)
+}
+
 class CalculatorButtonView: UIView {
-	private lazy var button: UILabel = {
-		let label = UILabel()
+	private lazy var button: UIButton = {
+		let button = UIButton()
 		
-		label.font = UIFont.systemFont(ofSize: 40, weight: .medium)
-		label.adjustsFontSizeToFitWidth = true
-		label.translatesAutoresizingMaskIntoConstraints = false
+		button.titleLabel?.adjustsFontSizeToFitWidth = true
+		button.titleLabel?.font = UIFont.systemFont(ofSize: 40, weight: .medium)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.addTarget(self, action: #selector(onTap), for: .touchUpInside)
 		
-		return label
+		return button
 	}()
 	
 	static var elementWidth = (UIScreen.main.bounds.width - 5 * 16) / 4
 	
-	init(text: String, textColor: UIColor? = .white, buttonColor: UIColor? = .darkGray, ignoreWidthConstraints: Bool = false, alignLeft: Bool = false) {
+	weak var delegate: CalculatorButtonViewDelegate?
+	
+	init() {
 		super.init(frame: .zero)
 		
-		backgroundColor = buttonColor
-		button.text = text
-		button.textColor = textColor
-		
-		addSubview(button)
-		
-		NSLayoutConstraint.activate([
-			button.centerYAnchor.constraint(equalTo: centerYAnchor),
-			heightAnchor.constraint(equalToConstant: CalculatorButtonView.elementWidth)
-		])
-		
-		if (!alignLeft) {
-			NSLayoutConstraint.activate([
-				button.centerXAnchor.constraint(equalTo: centerXAnchor),
-			])
-	
-		} else {
-			NSLayoutConstraint.activate([
-				button.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
-			])
-		}
-		
-		if (!ignoreWidthConstraints) {
-			NSLayoutConstraint.activate([
-				widthAnchor.constraint(equalToConstant: CalculatorButtonView.elementWidth)
-			])
-		}
-		layer.cornerRadius = CalculatorButtonView.elementWidth / 2
-		translatesAutoresizingMaskIntoConstraints = false
+		setup()
 	}
 	
 	required init?(coder: NSCoder) {
 		fatalError()
 	}
+	
+	@objc
+	private func onTap(sender: UIButton) {
+		delegate?.didTapButton(sender)
+	}
+	
+	func updateTitle(_ newTitle: String) {
+		button.setTitle(newTitle, for: .normal)
+	}
+	
+	func setup(
+		text: String,
+		textColor: UIColor? = .white,
+		buttonColor: UIColor? = .darkGray,
+		alignLeft: Bool = false,
+		width: CGFloat? = nil,
+		height: CGFloat? = nil
+	) {
+		backgroundColor = buttonColor
+		button.setTitle(text, for: .normal)
+		button.setTitleColor(textColor, for: .normal)
+		
+		if (!alignLeft) {
+			NSLayoutConstraint.activate([
+				button.centerXAnchor.constraint(equalTo: centerXAnchor),
+			])
+			
+		} else {
+			NSLayoutConstraint.activate([
+				button.leadingAnchor.constraint(equalTo: leadingAnchor),
+			])
+		}
+		
+		NSLayoutConstraint.activate([
+			widthAnchor.constraint(equalToConstant: width ?? CalculatorButtonView.elementWidth),
+			button.widthAnchor.constraint(equalToConstant: width ?? CalculatorButtonView.elementWidth),
+			button.heightAnchor.constraint(equalToConstant: height ?? CalculatorButtonView.elementWidth),
+		])
+	}
+}
+
+//MARK: - ViewCode
+
+extension CalculatorButtonView: ViewCode {
+	func addSubviews() {
+		addSubview(button)
+	}
+	
+	func setupConstrainsts() {
+		NSLayoutConstraint.activate([
+			button.centerYAnchor.constraint(equalTo: centerYAnchor),
+			heightAnchor.constraint(equalToConstant: CalculatorButtonView.elementWidth)
+		])
+	}
+	
+	func setupStyles() {
+		layer.cornerRadius = CalculatorButtonView.elementWidth / 2
+		translatesAutoresizingMaskIntoConstraints = false
+	}
+	
 }
--- Calculator/Features/Calculator/View/CalculatorView.swift
+++ Calculator/Features/Calculator/View/CalculatorView.swift
@@ -15,111 +15,193 @@ class CalculatorView: UIView {
 		label.font = UIFont.systemFont(ofSize: 80)
 		label.textColor = .white
 		label.translatesAutoresizingMaskIntoConstraints = false
+		label.textAlignment = .right
 		
 		return label
 	}()
 	
-	private lazy var one = CalculatorButtonView(text: "1")
-	private lazy var two = CalculatorButtonView(text: "2")
-	private lazy var three = CalculatorButtonView(text: "3")
-	private lazy var plus = CalculatorButtonView(text: "+", buttonColor: .orange)
-	private lazy var four = CalculatorButtonView(text: "4")
-	private lazy var five = CalculatorButtonView(text: "5")
-	private lazy var six = CalculatorButtonView(text: "6")
-	private lazy var minus = CalculatorButtonView(text: "-", buttonColor: .orange)
-	private lazy var seven = CalculatorButtonView(text: "7")
-	private lazy var eight = CalculatorButtonView(text: "8")
-	private lazy var nine = CalculatorButtonView(text: "9")
-	private lazy var multiply = CalculatorButtonView(text: "x", buttonColor: .orange)
-	private lazy var clear = CalculatorButtonView(text: "AC", textColor: .black, buttonColor: .lightGray)
-	private lazy var positiveNegative = CalculatorButtonView(text: "+/-", textColor: .black, buttonColor: .lightGray)
-	private lazy var percent = CalculatorButtonView(text: "%", textColor: .black, buttonColor: .lightGray)
-	private lazy var divide = CalculatorButtonView(text: "/", buttonColor: .orange)
-	private lazy var zero = CalculatorButtonView(text: "0", ignoreWidthConstraints: true, alignLeft: true)
-	private lazy var dot = CalculatorButtonView(text: ",")
-	private lazy var equal = CalculatorButtonView(text: "=", buttonColor: .orange)
+	weak var buttonsDelegate: CalculatorButtonViewDelegate? {
+		willSet {
+			one.delegate = newValue
+			two.delegate = newValue
+			three.delegate = newValue
+			plus.delegate = newValue
+			four.delegate = newValue
+			five.delegate = newValue
+			six.delegate = newValue
+			minus.delegate = newValue
+			seven.delegate = newValue
+			eight.delegate = newValue
+			nine.delegate = newValue
+			multiply.delegate = newValue
+			clear.delegate = newValue
+			positiveNegative.delegate = newValue
+			percent.delegate = newValue
+			divide.delegate = newValue
+			zero.delegate = newValue
+			comma.delegate = newValue
+			equal.delegate = newValue
+		}
+	}
+	
+	private lazy var one: CalculatorButtonView = {
+		let one = CalculatorButtonView()
+		one.setup(text: "1")
+		return one
+	}()
+	
+	private lazy var two: CalculatorButtonView = {
+		let two = CalculatorButtonView()
+		two.setup(text: "2")
+		return two
+	}()
+	
+	private lazy var three: CalculatorButtonView = {
+		let three = CalculatorButtonView()
+		three.setup(text: "3")
+		return three
+	}()
+	
+	private lazy var plus: CalculatorButtonView = {
+		let plus = CalculatorButtonView()
+		plus.setup(text: "+", buttonColor: .orange)
+		return plus
+	}()
+	
+	private lazy var four: CalculatorButtonView = {
+		let four = CalculatorButtonView()
+		four.setup(text: "4")
+		return four
+	}()
+	
+	private lazy var five: CalculatorButtonView = {
+		let five = CalculatorButtonView()
+		five.setup(text: "5")
+		return five
+	}()
+	
+	private lazy var six: CalculatorButtonView = {
+		let six = CalculatorButtonView()
+		six.setup(text: "6")
+		return six
+	}()
+	
+	private lazy var minus: CalculatorButtonView = {
+		let minus = CalculatorButtonView()
+		minus.setup(text: "-", buttonColor: .orange)
+		return minus
+	}()
+	
+	private lazy var seven: CalculatorButtonView = {
+		let seven = CalculatorButtonView()
+		seven.setup(text: "7")
+		return seven
+	}()
+	
+	private lazy var eight: CalculatorButtonView = {
+		let eight = CalculatorButtonView()
+		eight.setup(text: "8")
+		return eight
+	}()
+	
+	private lazy var nine: CalculatorButtonView = {
+		let nine = CalculatorButtonView()
+		nine.setup(text: "9")
+		return nine
+	}()
+	
+	private lazy var multiply: CalculatorButtonView = {
+		let multiply = CalculatorButtonView()
+		multiply.setup(text: "x", buttonColor: .orange)
+		return multiply
+	}()
+	
+	private lazy var clear: CalculatorButtonView = {
+		let clear = CalculatorButtonView()
+		clear.setup(text: "AC", textColor: .black, buttonColor: .lightGray)
+		return clear
+	}()
+	
+	private lazy var positiveNegative: CalculatorButtonView = {
+		let positiveNegative = CalculatorButtonView()
+		positiveNegative.setup(text: "+/-", textColor: .black, buttonColor: .lightGray)
+		return positiveNegative
+	}()
+	
+	private lazy var percent: CalculatorButtonView = {
+		let percent = CalculatorButtonView()
+		percent.setup(text: "%", textColor: .black, buttonColor: .lightGray)
+		return percent
+	}()
+	
+	private lazy var divide: CalculatorButtonView = {
+		let divide = CalculatorButtonView()
+		divide.setup(text: "/", buttonColor: .orange)
+		return divide
+	}()
+	
+	private lazy var zero: CalculatorButtonView = {
+		let zero = CalculatorButtonView()
+		zero.setup(text: "0", alignLeft: true, width: CalculatorButtonView.elementWidth * 2 + 16)
+		return zero
+	}()
+	
+	private lazy var comma: CalculatorButtonView = {
+		let comma = CalculatorButtonView()
+		comma.setup(text: ",")
+		return comma
+	}()
+	
+	private lazy var equal: CalculatorButtonView = {
+		let equal = CalculatorButtonView()
+		equal.setup(text: "=", buttonColor: .orange)
+		return equal
+	}()
 	
 	init() {
 		super.init(frame: .zero)
 		
 		backgroundColor = .black
-		
-		addSubviews()
-		putConstrains()
 	}
 	
 	required init?(coder: NSCoder) {
 		fatalError()
 	}
 	
-	private func addSubviews() {
-		addSubview(one)
-		addSubview(two)
-		addSubview(three)
-		addSubview(plus)
-		addSubview(four)
-		addSubview(five)
-		addSubview(six)
-		addSubview(minus)
-		addSubview(seven)
-		addSubview(eight)
-		addSubview(nine)
-		addSubview(multiply)
-		addSubview(clear)
-		addSubview(positiveNegative)
-		addSubview(percent)
-		addSubview(divide)
-		addSubview(zero)
-		addSubview(dot)
-		addSubview(equal)
-		addSubview(result)
-	}
-	
-	private func putConstrains() {
-		oneConstraints()
-		twoConstraints()
-		threeConstraints()
-		fourConstraints()
-		plusConstraints()
-		fiveConstraints()
-		sixConstraints()
-		minusConstraints()
-		sevenConstraints()
-		eightConstraints()
-		nineConstraints()
-		multiplyConstraints()
-		clearConstraints()
-		positiveNegativeConstraints()
-		percentConstraints()
-		devideConstraints()
-		zeroConstraints()
-		dotConstraints()
-		equalConstraints()
-		resultConstraints()
+	func updateResult(_ newValue: Double) {
+		let justInt = floor(newValue)
+		let isInt = justInt == newValue
+		
+		if (isInt) {
+			result.text = String(Int(justInt))
+		} else {
+			result.text = String(newValue)
+		}
+		
+		clear.updateTitle(result.text == "0" ? "AC" : "C")
 	}
 	
 	private func zeroConstraints() {
 		NSLayoutConstraint.activate([
 			zero.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
 			zero.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -24),
-			zero.widthAnchor.constraint(equalToConstant: CalculatorButtonView.elementWidth * 2 + 16)
 		])
 	}
-
-	private func dotConstraints() {
+	
+	private func commaConstraints() {
 		NSLayoutConstraint.activate([
-			dot.leadingAnchor.constraint(equalTo: zero.trailingAnchor, constant: 16),
-			dot.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -24),
+			comma.leadingAnchor.constraint(equalTo: zero.trailingAnchor, constant: 16),
+			comma.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -24),
 		])
 	}
-
+	
 	private func equalConstraints() {
 		NSLayoutConstraint.activate([
-			equal.leadingAnchor.constraint(equalTo: dot.trailingAnchor, constant: 16),
+			equal.leadingAnchor.constraint(equalTo: comma.trailingAnchor, constant: 16),
 			equal.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -24),
 		])
 	}
-
+	
 	private func oneConstraints() {
 		NSLayoutConstraint.activate([
 			one.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
@@ -235,7 +317,60 @@ class CalculatorView: UIView {
 	private func resultConstraints() {
 		NSLayoutConstraint.activate([
 			result.bottomAnchor.constraint(equalTo: divide.topAnchor, constant: -24),
-			result.trailingAnchor.constraint(equalTo: divide.trailingAnchor, constant: -16)
+			result.trailingAnchor.constraint(equalTo: divide.trailingAnchor, constant: -16),
+			result.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
 		])
+	}
+}
+
+//MARK: - ViewCode
+
+extension CalculatorView: ViewCode {
+	func setupStyles() {}
+	
+	func setupConstrainsts() {
+		oneConstraints()
+		twoConstraints()
+		threeConstraints()
+		fourConstraints()
+		plusConstraints()
+		fiveConstraints()
+		sixConstraints()
+		minusConstraints()
+		sevenConstraints()
+		eightConstraints()
+		nineConstraints()
+		multiplyConstraints()
+		clearConstraints()
+		positiveNegativeConstraints()
+		percentConstraints()
+		devideConstraints()
+		zeroConstraints()
+		commaConstraints()
+		equalConstraints()
+		resultConstraints()
+	}
+
+	func addSubviews() {
+		addSubview(one)
+		addSubview(two)
+		addSubview(three)
+		addSubview(plus)
+		addSubview(four)
+		addSubview(five)
+		addSubview(six)
+		addSubview(minus)
+		addSubview(seven)
+		addSubview(eight)
+		addSubview(nine)
+		addSubview(multiply)
+		addSubview(clear)
+		addSubview(positiveNegative)
+		addSubview(percent)
+		addSubview(divide)
+		addSubview(zero)
+		addSubview(comma)
+		addSubview(equal)
+		addSubview(result)
 	}
 }
